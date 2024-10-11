e92ac96360f0653f4e17c774ed016bbf79f70818
Raul <ibragraul@gmail.com>
2024-10-11 22:25:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Funktion zur Navigation zwischen Seiten
-function goto(page) {
-    window.location.href = page;
+function goto(page, meal) {
+    window.location.href = `${page}?meal=${meal}`;
+}
+
+// Funktion zum Auslesen der URL-Parameter
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Den Parameter "meal" auslesen
+    const meal = getQueryParam('meal');
+
+// Wenn ein "meal" Parameter gefunden wurde, wird der Titel geändert
+    if (meal) {
+        document.getElementById('page-title').innerText = meal;
+        document.getElementById('heading-title').innerText = meal;
 }
