567e875325198259f1ee49ff76b665a8c8a29e41
Raul <ibragraul@gmail.com>
2024-10-12 00:24:45 +0200

    Update ddjs.js

--- ddjs.js
+++ ddjs.js
@@ -27,15 +27,30 @@ window.onload = function () {
 function displayZutaten(index) {
     const zutaten = zutatenData[index];
     if (zutaten) {
-        const img = document.getElementById('zutaten-image');
-        img.src = ''; // Zurücksetzen des src-Attributs
-        img.src = zutaten.img; // Neues Bild laden
-        img.onerror = function() {
-            this.src = 'img/placeholder.jpg'; // Fallback-Bild, falls das Laden fehlschlägt
-        };
         document.getElementById('zutaten-name').textContent = zutaten.name;
         document.getElementById('zutaten-tags').textContent = zutaten.tags.join(', ');
+        loadImage(zutaten.img);
     }
+}
+
+function loadImage(src) {
+    const img = document.getElementById('zutaten-image');
+    const placeholder = 'img/placeholder.jpg';
+
+    // Zuerst versuchen wir, das Bild direkt zu laden
+    img.src = src;
+
+    img.onerror = function() {
+        // Wenn das direkte Laden fehlschlägt, versuchen wir es mit einem Zeitstempel
+        const timestamp = new Date().getTime();
+        img.src = `${src}?t=${timestamp}`;
+        
+        img.onerror = function() {
+            // Wenn auch das nicht klappt, laden wir das Platzhalterbild
+            console.error(`Fehler beim Laden des Bildes: ${src}`);
+            img.src = placeholder;
+        };
+    };
 }
 
 function addEventListeners() {
